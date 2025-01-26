30f45b10a708236b33c22fffed798b3dd9ad1438
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-01-26 20:03:07 +0800

    Domain领域初始化

---
+++ Domain/DAO/src/main/java/xyz/spc/domain/DAOApplication.java
@@ -0,0 +1,8 @@
+package xyz.spc.domain;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DAOApplication {
+
+}
---
+++ Domain/Repo/src/main/java/xyz/spc/domain/RepoApplication.java
@@ -0,0 +1,7 @@
+package xyz.spc.domain;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoApplication {
+}

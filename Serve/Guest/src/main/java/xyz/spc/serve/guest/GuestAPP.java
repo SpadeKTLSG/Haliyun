371a69d82e78764c2855c3fac2ce9893b17f319e
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-01-29 19:51:10 +0800

    fix pom + properties

--- Serve/Guest/src/main/java/xyz/spc/serve/guest/GuestAPP.java
+++ Serve/Guest/src/main/java/xyz/spc/serve/guest/GuestAPP.java
@@ -1,5 +1,6 @@
 package xyz.spc.serve.guest;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients(basePackages = "xyz.spc.infra.feign.Group") //开启Feign客户端
 @EnableDiscoveryClient //开启服务发现
 @SpringBootApplication
+@MapperScan("xyz.spc.infra") //扫描Mapper接口
 public class GuestAPP {
     public static void main(String[] args) {
         SpringApplication.run(GuestAPP.class, args);

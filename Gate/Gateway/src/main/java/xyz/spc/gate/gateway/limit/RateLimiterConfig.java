74ecbb3a6bbdee3ec2310271f09db774ce07a719
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-02-03 21:43:35 +0800

    亮点混录 7 手写令牌桶等待 - 网关限流器ok

---
+++ Gate/Gateway/src/main/java/xyz/spc/gate/gateway/limit/RateLimiterConfig.java
@@ -0,0 +1,23 @@
+package xyz.spc.gate.gateway.limit;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+/**
+ * 网关限流配置
+ */
+@Configuration
+public class RateLimiterConfig {
+
+    /**
+     * IP限流器
+     */
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress());
+    }
+}

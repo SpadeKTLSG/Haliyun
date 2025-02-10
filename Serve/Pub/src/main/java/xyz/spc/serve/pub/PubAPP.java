0ff3d090d17705911cb77cb4839b9512d6889834
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-02-10 22:13:25 +0800

    [All]/users 1-5 登陆模块: 用户登陆前端联调 5 菜单List制作+落库 全导入

--- Serve/Pub/src/main/java/xyz/spc/serve/pub/PubAPP.java
+++ Serve/Pub/src/main/java/xyz/spc/serve/pub/PubAPP.java
@@ -1,7 +1,30 @@
 package xyz.spc.serve.pub;
 
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * 公共服务启动类
+ */
+@Slf4j
+@MapperScan({"xyz.spc.infra.mapper"})
+@SpringBootApplication(scanBasePackages = {
+        "xyz.spc.serve.pub",
+        "xyz.spc.serve.auxiliary",
+        "xyz.spc.infra.*",
+        "xyz.spc.common.*"})
+@EnableFeignClients(basePackages = "xyz.spc.infra.feign")
+@EnableDiscoveryClient //开启服务发现
+@EnableScheduling //开启定时任务
+@EnableAspectJAutoProxy() //开启AspectJ
+@EnableRetry //开启重试
 public class PubAPP {
     public static void main(String[] args) {
         SpringApplication.run(PubAPP.class, args);

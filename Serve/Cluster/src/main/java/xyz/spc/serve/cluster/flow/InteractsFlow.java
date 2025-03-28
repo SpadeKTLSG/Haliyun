9b4914e4a62c24bbdcdf987c2b394782f8b33248
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-03-28 12:17:09 +0800

    Guest 3 用户数据 - 收藏App Post动态 分页  小补充 cluster 替代group 是听不懂吗玄桃K

--- Serve/Cluster/src/main/java/xyz/spc/serve/cluster/flow/InteractsFlow.java
+++ Serve/Cluster/src/main/java/xyz/spc/serve/cluster/flow/InteractsFlow.java
@@ -14,6 +14,7 @@ import xyz.spc.serve.cluster.func.interacts.PostFunc;
 import xyz.spc.serve.cluster.func.interacts.RemarkFunc;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -39,22 +40,42 @@ public class InteractsFlow {
                 tmp.getTotal()
         );
 
-        //补充信息
-        for (PostDO post : tmp.getRecords()) {
-            PostShowVO postShowVO = new PostShowVO();
-            postShowVO.setId(post.getId());
-            postShowVO.setUserId(post.getUserId());
-            postShowVO.setContent(post.getContent());
-            postShowVO.setCreateTime(post.getCreateTime());
-            postShowVO.setUpdateTime(post.getUpdateTime());
-            postShowVO.setTitle(post.getTitle());
-            postShowVO.setPics(post.getPics());
-            postShowVO.setPersonShow(post.getPersonShow());
-            //这个要改一次性批量调用
-            postShowVO.setClusterName(clustersFunc.getClusterNamesByIds(List.of(post.getClusterId())).get(0));
+
+        // 收集所有需要查询的 clusterId
+        List<Long> clusterIds = tmp.getRecords().stream()
+                .map(PostDO::getClusterId)
+                .collect(Collectors.toList());
+
+        // 批量查询 clusterName
+        List<String> clusterNames = clustersFunc.getClusterNamesByIds(clusterIds);
+
+        // 补充信息
+        for (int i = 0; i < tmp.getRecords().size(); i++) {
+            PostShowVO postShowVO = getPostShowVO(tmp, i, clusterNames);
             res.getRecords().add(postShowVO);
         }
 
         return res;
+    }
+
+
+    /**
+     * 补充 PostShowVO 信息
+     */
+    private static PostShowVO getPostShowVO(PageResponse<PostDO> tmp, int i, List<String> clusterNames) {
+        PostDO post = tmp.getRecords().get(i);
+
+        PostShowVO postShowVO = new PostShowVO();
+        postShowVO.setId(post.getId());
+        postShowVO.setUserId(post.getUserId());
+        postShowVO.setContent(post.getContent());
+        postShowVO.setCreateTime(post.getCreateTime());
+        postShowVO.setUpdateTime(post.getUpdateTime());
+        postShowVO.setTitle(post.getTitle());
+        postShowVO.setPics(post.getPics());
+        postShowVO.setPersonShow(post.getPersonShow());
+        postShowVO.setClusterName(clusterNames.get(i));
+
+        return postShowVO;
     }
 }

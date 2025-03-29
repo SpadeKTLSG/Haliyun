2e0809b73e002a0473710ea30b0fceded165d95f
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-03-29 16:40:09 +0800

    Guest 5 用户数据 - 收藏App 群组 日期处理问题

--- Common/src/main/java/xyz/spc/common/funcpack/page/PageRequest.java
+++ Common/src/main/java/xyz/spc/common/funcpack/page/PageRequest.java
@@ -1,6 +1,5 @@
 package xyz.spc.common.funcpack.page;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import xyz.spc.common.constant.ReqRespCT;
 
@@ -8,16 +7,33 @@ import xyz.spc.common.constant.ReqRespCT;
  * 分页请求对象 (别再拿来放接口了, 维护性太烂)
  */
 @Data
-@AllArgsConstructor
 public class PageRequest {
+
 
     /**
      * 当前页
      */
-    private Integer current = 1;
+    private Integer current;
 
     /**
      * 每页显示条数
      */
-    private Integer size = ReqRespCT.DEFAULT_PAGE_SIZE;
+    private Integer size;
+
+
+    public PageRequest() {
+        new PageRequest(1, ReqRespCT.DEFAULT_PAGE_SIZE);
+    }
+
+    public PageRequest(Integer current) {
+        this.size = ReqRespCT.DEFAULT_PAGE_SIZE;
+        new PageRequest(current, size);
+    }
+
+    public PageRequest(Integer current, Integer size) {
+        this.current = current;
+        this.size = size;
+    }
+
+
 }

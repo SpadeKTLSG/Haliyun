39b9ed68d5f9f799a8e68999d761cf7a4f6a3b65
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-04-06 12:19:12 +0800

    群组公告 2 接口

--- Serve/Cluster/src/main/java/xyz/spc/serve/cluster/controller/functions/NoticeControl.java
+++ Serve/Cluster/src/main/java/xyz/spc/serve/cluster/controller/functions/NoticeControl.java
@@ -1,4 +1,21 @@
 package xyz.spc.serve.cluster.controller.functions;
 
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.spc.serve.auxiliary.config.log.MLog;
+
+@Slf4j
+@MLog
+@Tag(name = "Clusters", description = "群组功能合集")
+@RequestMapping("/Cluster/functions")
+@RestController
+@RequiredArgsConstructor
 public class NoticeControl {
+
+
+
 }

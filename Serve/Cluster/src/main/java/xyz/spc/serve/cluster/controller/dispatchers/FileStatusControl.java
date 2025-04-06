5de728a1d053613a5109a62e0e3b2a816d404fbd
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-04-06 23:39:32 +0800

    群组公告 2 接口

--- Serve/Cluster/src/main/java/xyz/spc/serve/cluster/controller/dispatchers/FileStatusControl.java
+++ Serve/Cluster/src/main/java/xyz/spc/serve/cluster/controller/dispatchers/FileStatusControl.java
@@ -1,4 +1,17 @@
 package xyz.spc.serve.cluster.controller.dispatchers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.spc.serve.auxiliary.config.log.MLog;
+
+@Slf4j
+@MLog
+@Tag(name = "Clusters", description = "群组调度合集")
+@RequestMapping("/Cluster/dispatchers")
+@RestController
+@RequiredArgsConstructor
 public class FileStatusControl {
 }
--- Serve/Cluster/src/main/java/xyz/spc/serve/cluster/controller/managers/ClusterAuthControl.java
+++ Serve/Cluster/src/main/java/xyz/spc/serve/cluster/controller/managers/ClusterAuthControl.java
@@ -1,4 +1,17 @@
 package xyz.spc.serve.cluster.controller.managers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.spc.serve.auxiliary.config.log.MLog;
+
+@Slf4j
+@MLog
+@Tag(name = "Clusters", description = "群组管理合集")
+@RequestMapping("/Cluster/managers")
+@RestController
+@RequiredArgsConstructor
 public class ClusterAuthControl {
 }

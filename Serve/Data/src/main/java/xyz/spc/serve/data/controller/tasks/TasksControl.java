327136cc9c0f58c14ce74a57fd19ae11a48e53af
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-04-08 00:10:31 +0800

    群组文件* 2 基础上传 实现

--- Serve/Data/src/main/java/xyz/spc/serve/data/controller/tasks/TasksControl.java
+++ Serve/Data/src/main/java/xyz/spc/serve/data/controller/tasks/TasksControl.java
@@ -1,4 +1,24 @@
 package xyz.spc.serve.data.controller.tasks;
 
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.spc.serve.auxiliary.config.log.MLog;
+
+@Slf4j
+@MLog
+@Tag(name = "Tasks", description = "文件任务合集")
+@RequestMapping("/Data/tasks")
+@RestController
+@RequiredArgsConstructor
 public class TasksControl {
+
+
+
+//    upload/file
+
+
 }

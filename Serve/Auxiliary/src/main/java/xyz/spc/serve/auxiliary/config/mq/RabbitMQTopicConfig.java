a6d9d18911811a9730961282dbdfaae822ad8032
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-04-08 18:24:19 +0800

    群组文件* 2  消息队列绑定

--- Serve/Auxiliary/src/main/java/xyz/spc/serve/auxiliary/config/mq/RabbitMQTopicConfig.java
+++ Serve/Auxiliary/src/main/java/xyz/spc/serve/auxiliary/config/mq/RabbitMQTopicConfig.java
@@ -24,6 +24,19 @@ public class RabbitMQTopicConfig {
         return new Queue(BasicMQCompo.BASIC_QUEUE);
     }
 
+
+    // 上传任务使用消息队列
+    @Bean
+    public Queue tasksUploadQueue() {
+        return new Queue("tasks_upload_queue");
+    }
+
+    // 下载任务使用消息队列
+    @Bean
+    public Queue tasksDownloadQueue() {
+        return new Queue("tasks_download_queue");
+    }
+
     /**
      * 交换机
      */
@@ -32,12 +45,40 @@ public class RabbitMQTopicConfig {
         return new FanoutExchange(BasicMQCompo.BASIC_EXCHANGE);
     }
 
+    // 上传任务使用交换机
+    @Bean
+    public FanoutExchange tasksUploadExchange() {
+        return new FanoutExchange("tasks_upload_exchange");
+    }
+
+
+    // 下载任务使用交换机
+    @Bean
+    public FanoutExchange tasksDownloadExchange() {
+        return new FanoutExchange("tasks_download_exchange");
+    }
+
+
+
     /**
      * 绑定
      */
     @Bean
     public Binding binding() {
         return BindingBuilder.bind(haliQueue()).to(haliExchange());
+    }
+
+
+    // 上传任务绑定
+    @Bean
+    public Binding uploadBinding() {
+        return BindingBuilder.bind(tasksUploadQueue()).to(tasksUploadExchange());
+    }
+
+    // 下载任务绑定
+    @Bean
+    public Binding downloadBinding() {
+        return BindingBuilder.bind(tasksDownloadQueue()).to(tasksDownloadExchange());
     }
 
 }

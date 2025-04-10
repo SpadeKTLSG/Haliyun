03761691d3e130011e470bc99f144c1ee19cdf26
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-04-10 11:28:17 +0800

    群组文件* 7 分页 测试 + Detail

--- Serve/Data/src/main/java/xyz/spc/serve/data/flow/TasksFlow.java
+++ Serve/Data/src/main/java/xyz/spc/serve/data/flow/TasksFlow.java
@@ -4,7 +4,9 @@ package xyz.spc.serve.data.flow;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import xyz.spc.gate.dto.Data.files.FileDTO;
 import xyz.spc.infra.feign.Cluster.ClustersClient;
@@ -39,12 +41,13 @@ public class TasksFlow {
      * @param file      上传的文件对象
      * @param clusterId 群组id
      */
+    @Transactional(rollbackFor = Exception.class, timeout = 50)
     public void uploadFile(MultipartFile file, Long clusterId, Long userId) throws IOException {
 
-        // 实现上传文件中转存储对应机器位置
+        // 1 实现上传文件中转存储对应机器位置
         String localFilePath = uploadTaskFunc.handleTempUpload(file, userId, clusterId);
 
-        // 创建对应的文件对象 (临时) - 组装入参
+        // 2 创建对应的文件对象 (临时) - 组装入参
         String type = Objects.requireNonNull(file.getOriginalFilename()).substring(file.getOriginalFilename().lastIndexOf(".") + 1);
 
         FileDTO tmp = FileDTO.builder()
@@ -56,16 +59,29 @@ public class TasksFlow {
 
         Long file_id = filesFunc.createFile(tmp, file.getSize());
 
-        // 任务记录创建到批处理表, 生成对应记录
+        // 3 任务记录创建到批处理表, 生成对应记录
         Long taskId = uploadTaskFunc.taskGen(file_id, file.getOriginalFilename(), userId);
 
-        // 简单封装任务id和本地文件路径到发送的消息中
-
-        // 调用 MQ 发送消息, 消息仅仅包含 Long 任务id, 直接从数据库找出对应行的任务进行异步的 本地缓存 -> HDFS 即可
+        // 4 简单封装任务id和本地文件路径到发送的消息中
+        // 调用 MQ 发送消息, 从数据库找出对应行的任务进行异步的 本地缓存 -> HDFS 即可
         mqProducer.convertAndSend(
                 TasksMQCompo.UPLOAD_EXCHANGE,
                 "", //简单绑定不需要 routingKey
                 new Object[]{taskId, localFilePath}
         );
+
+
+        //? note 这里涉及到分布式事务问题. 如果上传 HDFS 失败了, 那么任务处理表状态应该是失败的, 但是文件表却写了进去
+        // 因此, 我们需要在上传 HDFS 失败的情况下, 将文件表的记录删除掉(回滚)
+    }
+
+
+    /**
+     * 上传文件失败后的分布式事务回滚: 最终一致性实现
+     */
+    @Async
+    public void rollBackFileCreate4Failure(Long fileId) {
+        // 删除文件表的记录
+        filesFunc.deleteFile(fileId);
     }
 }

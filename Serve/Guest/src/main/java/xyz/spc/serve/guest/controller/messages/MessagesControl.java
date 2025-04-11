08c17bebbde5c6d666f75eb0f9261ff7a601cd3d
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-04-11 17:32:28 +0800

    用户消息 2 消息列表查询

--- Serve/Guest/src/main/java/xyz/spc/serve/guest/controller/messages/MessagesControl.java
+++ Serve/Guest/src/main/java/xyz/spc/serve/guest/controller/messages/MessagesControl.java
@@ -3,10 +3,16 @@ package xyz.spc.serve.guest.controller.messages;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.spc.common.funcpack.Result;
+import xyz.spc.gate.vo.Guest.messages.SelfMailVO;
 import xyz.spc.serve.auxiliary.config.log.MLog;
 import xyz.spc.serve.guest.flow.MessagesFlow;
+
+import java.util.List;
 
 @Slf4j
 @MLog
@@ -34,5 +40,17 @@ public class MessagesControl {
 
     //! UPDATE
 
+
     //! Query
+
+
+    /**
+     * 用户消息列表 - 收件箱 / 发件箱
+     * (直接查询全部, 没有分页)
+     * ? 使用入参来判断是收件箱 (0) - 默认 还是发件箱 (1)
+     */
+    @GetMapping("/list")
+    public Result<List<SelfMailVO>> listMyMes(@RequestParam Integer orderType) {
+        return Result.success(messagesFlow.listMyMes(orderType));
+    }
 }
--- Serve/Guest/src/main/java/xyz/spc/serve/guest/flow/MessagesFlow.java
+++ Serve/Guest/src/main/java/xyz/spc/serve/guest/flow/MessagesFlow.java
@@ -3,10 +3,14 @@ package xyz.spc.serve.guest.flow;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import xyz.spc.domain.dos.Guest.messages.SelfMailDO;
+import xyz.spc.gate.vo.Guest.messages.SelfMailVO;
 import xyz.spc.infra.feign.Guest.UsersClient;
-import xyz.spc.serve.guest.func.records.StatisticFunc;
-import xyz.spc.serve.guest.func.records.TombFunc;
+import xyz.spc.serve.auxiliary.common.context.UserContext;
+import xyz.spc.serve.guest.func.messages.SelfMailFunc;
 import xyz.spc.serve.guest.func.users.UsersFunc;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -18,7 +22,29 @@ public class MessagesFlow {
 
     //Func
     private final UsersFunc usersFunc;
-    private final TombFunc tombFunc;
-    private final StatisticFunc statisticFunc;
+    private final SelfMailFunc selfMailFunc;
 
+
+
+
+    /**
+     * 查看我的消息列表
+     */
+    public List<SelfMailVO> listMyMes(Integer orderType) {
+
+        // 1 获取用户ID
+        Long userId = UserContext.getUI();
+
+
+        // 2 查询 发件箱 / 收件箱 原生字段
+        List<SelfMailDO> tmp = selfMailFunc.listMyMes(userId, orderType);
+
+        // 3 补充 群组 名称
+
+
+        // 4 补充 发件人 + 收件人 名称
+
+
+        // 5 (异步) 邮件Status字段状态更新
+    }
 }

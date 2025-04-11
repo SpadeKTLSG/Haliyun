5299125a05eedab7be12135716aaf2c54abd244d
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-04-11 22:40:26 +0800

    用户消息 2 消息列表 详情 (阅读) 业务

--- Serve/Guest/src/main/java/xyz/spc/serve/guest/flow/MessagesFlow.java
+++ Serve/Guest/src/main/java/xyz/spc/serve/guest/flow/MessagesFlow.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import xyz.spc.common.funcpack.errorcode.ServerError;
 import xyz.spc.common.funcpack.exception.ServiceException;
 import xyz.spc.domain.dos.Guest.messages.SelfMailDO;
+import xyz.spc.domain.model.Guest.messages.SelfMail;
 import xyz.spc.gate.vo.Guest.messages.SelfMailVO;
 import xyz.spc.infra.feign.Cluster.ClustersClient;
 import xyz.spc.infra.feign.Guest.UsersClient;
@@ -107,17 +108,60 @@ public class MessagesFlow {
         // 1 查询原生字段
         SelfMailDO tmp = selfMailFunc.getMyMesDetailById(mesId);
 
-        // 2 补充 群组名称
+        // 2 补充 群组名称 (复用)
+        Long clusterId = tmp.getClusterId();
+        List<Long> clusterIds = List.of(clusterId);
 
-        // 3 补充 发件人 + 收件人 名称
+        String clusterName = clustersClient.getClusterNamesByIds(clusterIds).get(0);
+
+        // 3 补充 发件人 + 收件人 名称  (复用)
+        Long senderId = tmp.getSenderId();
+        Long receiverId = tmp.getReceiverId();
+        List<Long> userIds = List.of(senderId, receiverId);
+
+        List<String> userAccountByIds = usersFunc.getUserAccountByIds(userIds);
+        String senderName = userAccountByIds.get(userIds.indexOf(senderId));
+        String receiverName = userAccountByIds.get(userIds.indexOf(receiverId));
+
 
         // 4 组装, 拼装 Tmodel
+        SelfMailVO res = SelfMailVO.builder()
+                .id(tmp.getId())
+
+                // 发件者信息
+                .senderId(senderId)
+                .senderName(senderName)
+
+                // 收件者信息
+                .receiverId(receiverId)
+                .receiverName(receiverName)
+
+                // 群组信息
+                .clusterId(clusterId)
+                .clusterName(clusterName)
+
+                // 信件信息
+                .header(tmp.getHeader())
+                .body(tmp.getBody())
+
+                // 信件状态
+                .status(tmp.getStatus())
+                .drop(tmp.getDrop())
+
+                .build();
+
 
         // 5 逻辑修改 tmp 的对象为字段更新后的结果
+        if (orderType == 1 && tmp.getStatus().equals(SelfMail.STATUS_DELIVER)) {
+            tmp.setStatus(SelfMail.STATUS_READ); //设置已阅
+        }
 
         // 6 (异步) 反向批量邮件Status字段状态更新对应落库 (区分 orderType)
+        List<SelfMailDO> selfMailDOS = List.of(tmp);
+        selfMailFunc.updateMesStatusSpecial(selfMailDOS, null, orderType);
+
+        return res;
     }
 
 
-    //        selfMailFunc.updateMesStatus();
 }
--- Serve/Guest/src/main/java/xyz/spc/serve/guest/func/messages/SelfMailFunc.java
+++ Serve/Guest/src/main/java/xyz/spc/serve/guest/func/messages/SelfMailFunc.java
@@ -29,37 +29,39 @@ public class SelfMailFunc {
      * 批量更新邮件状态为指定状态
      */
     @Async
-    public void updateMesStatus(List<SelfMailDO> selfMailDOS, Integer targetStatus, Integer orderType) {
+    public void updateMesStatusSpecial(List<SelfMailDO> selfMailDOS, Integer targetStatus, Integer orderType) {
 
-        //1 将需要更新的邮件状态设置为目标状态
+        //1 将需要更新的邮件状态设置为目标状态 (如果没传参tS, 就不执行下面的判断逻辑)
+        if (targetStatus != null) {
 
-        //2 加入判断: 对应状态不能回滚. 区分发件收件
-        selfMailDOS.forEach(
+            //2 加入判断: 对应状态不能回滚. 区分发件收件
+            selfMailDOS.forEach(
 
-                selfMailDO -> {
+                    selfMailDO -> {
 
-                    if (orderType == 0) {
-                        // 0 收件箱, 可选值只能修改为 已查看 || 已投递未查看
-                        if (targetStatus != SelfMail.STATUS_READ && targetStatus != SelfMail.STATUS_DELIVER) {
-                            throw new ServiceException(ServerError.SERVICE_ILLEGAL_PARAM_ERROR);
+                        if (orderType == 0) {
+                            // 0 收件箱, 可选值只能修改为 已查看 || 已投递未查看
+                            if (targetStatus != SelfMail.STATUS_READ && targetStatus != SelfMail.STATUS_DELIVER) {
+                                throw new ServiceException(ServerError.SERVICE_ILLEGAL_PARAM_ERROR);
+                            }
+                            selfMailDO.setStatus(targetStatus);
+
+                        } else if (orderType == 1) {
+                            // 1 发件箱, 可选值只能修改为 创建未发送 || 已发送未投递
+                            if (targetStatus != SelfMail.STATUS_SEND && targetStatus != SelfMail.STATUS_CREATE) {
+                                throw new ServiceException(ServerError.SERVICE_ILLEGAL_PARAM_ERROR);
+                            }
+                            selfMailDO.setStatus(targetStatus);
+
+                        } else {
+                            throw new ServiceException(ServerError.SERVICE_ILLEGAL_ERROR);
                         }
-                        selfMailDO.setStatus(targetStatus);
 
-                    } else if (orderType == 1) {
-                        // 1 发件箱, 可选值只能修改为 创建未发送 || 已发送未投递
-                        if (targetStatus != SelfMail.STATUS_SEND && targetStatus != SelfMail.STATUS_CREATE) {
-                            throw new ServiceException(ServerError.SERVICE_ILLEGAL_PARAM_ERROR);
-                        }
-                        selfMailDO.setStatus(targetStatus);
-
-                    } else {
-                        throw new ServiceException(ServerError.SERVICE_ILLEGAL_ERROR);
                     }
+            );
+        } // 没有传参的时候直接更新为对应的即可 (id查)
 
-                }
-        );
-
-        // 3 批量更新
+        // 2 批量更新
         selfMailsRepo.selfMailService.updateBatchById(selfMailDOS);
 
     }
